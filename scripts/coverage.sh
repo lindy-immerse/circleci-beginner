32ec62c9075718bf748ca5fbeb47181f7548df22
Lindy Brits <lindy.brits@immerse.io>
2020-02-03 14:24:02 +0000

    Changes made.

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -19,11 +19,29 @@ JSON=$(jq -n \
 
 ##------ PULL COVERAGE ARTIFACT ------##
 
-ARTIFACT=$(curl -L "https://circleci.com/api/v1.1/project/github/lindy-immerse/circleci-beginner/latest/artifacts?circle-token=b045cdea55d0e571a208181e4366f4c62c4016ce")
-echo "$ARTIFACT"
+PIPELINES=$(curl -L "https://circleci.com/api/v2/project/github/lindy-immerse/circleci-beginner/pipeline?circle-token=b045cdea55d0e571a208181e4366f4c62c4016ce")
+LATEST_PIPELINE_ID=$(echo "$PIPELINES" | jq '.items' | jq '.[0]' | jq -r '.id')
+LATEST_WORKFLOWS=$(curl -L "https://circleci.com/api/v2/pipeline/$LATEST_PIPELINE_ID/workflow?circle-token=b045cdea55d0e571a208181e4366f4c62c4016ce")
+LATEST_WORKFLOW_ID=$(echo "$LATEST_WORKFLOWS" | jq '.items' | jq '.[0]' | jq -r '.id')
+LATEST_JOBS=$(curl -L "https://circleci.com/api/v2/workflow/$LATEST_WORKFLOW_ID/job?circle-token=b045cdea55d0e571a208181e4366f4c62c4016ce")
+JOB_NUMBER=$(echo "$LATEST_JOBS" | jq '.items' | jq '.[] | select(.name=="angular_testing")' | jq -r '.job_number')
+ARTIFACT_DETAILS=$(curl -L "https://circleci.com/api/v2/project/github/lindy-immerse/circleci-beginner/$JOB_NUMBER/artifacts?circle-token=b045cdea55d0e571a208181e4366f4c62c4016ce")
+ARTIFACT_URL=$(echo "$ARTIFACT_DETAILS" | jq '.items' | jq '.[0]' | jq -r '.url')
+ARTIFACT=$(curl -L $ARTIFACT_URL)
 
-##------ GET NEW COVERAGE ------##
+echo -e "\n"
+echo "---> LATEST PIPELINE ---> $LATEST_PIPELINE_ID"
+echo "---> LATEST WORKFLOW ---> $LATEST_WORKFLOW_ID"
+echo "---> JOB NUMBER ---> $JOB_NUMBER"
+echo "---> ARTIFACT URL ---> $ARTIFACT_URL"
+echo -e "---> ARTIFACT\n$ARTIFACT"
+echo -e "\n"
+
+##------ DETERMINE NEW THRESHOLD ------##
+
+##------ SAVE NEW THRESHOLD ------##
+
 
 EPOCH_TIME=$(date +%s)
 echo "$JSON" > "$PWD/data/karma_stats_$EPOCH_TIME.json"
-echo "$JSON"
+#echo "$JSON"
