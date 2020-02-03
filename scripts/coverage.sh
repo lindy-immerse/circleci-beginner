0b4ecd44f7628ac288846178310c66bbf9c871f5
Lindy Brits <lindy.brits@immerse.io>
2020-02-03 12:06:18 +0000

    Changes made.

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+##------ GET CURRENT COVERAGE ------##
+
 COVERAGE_FILE=$(cat "../circleci/coverage/circleci/coverage-summary.json" | jq '.')
 COVERAGE_STATS=$(echo "$COVERAGE_FILE" | jq '.total')
 
@@ -14,6 +16,13 @@ JSON=$(jq -n \
         --arg functions "$FUNCTIONS" \
         --arg branches "$BRANCHES" \
         '{lines: $lines, statements: $statements, functions: $functions, branches: $branches}')
+
+##------ PULL COVERAGE ARTIFACT ------##
+
+ARTIFACT=$(curl -L "https://circleci.com/api/v1.1/project/github/lindy-immerse/circleci-beginner/latest/artifacts?circle-token=b045cdea55d0e571a208181e4366f4c62c4016ce")
+echo "$ARTIFACT"
+
+##------ GET NEW COVERAGE ------##
 
 EPOCH_TIME=$(date +%s)
 echo "$JSON" > "$PWD/data/karma_stats_$EPOCH_TIME.json"
