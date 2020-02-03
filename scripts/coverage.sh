f85587ccab0498f9232d4c8659ba16dd8cae610b
Lindy Brits <lindy.brits@immerse.io>
2020-02-03 14:27:35 +0000

    Changes made.

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -17,14 +17,14 @@ JSON=$(jq -n \
         --arg branches "$BRANCHES" \
         '{lines: $lines, statements: $statements, functions: $functions, branches: $branches}')
 
-##------ PULL COVERAGE ARTIFACT ------##
+##------ PULL THRESHOLD ARTIFACT ------##
 
 PIPELINES=$(curl -L "https://circleci.com/api/v2/project/github/lindy-immerse/circleci-beginner/pipeline?circle-token=b045cdea55d0e571a208181e4366f4c62c4016ce")
-LATEST_PIPELINE_ID=$(echo "$PIPELINES" | jq '.items' | jq '.[0]' | jq -r '.id')
-LATEST_WORKFLOWS=$(curl -L "https://circleci.com/api/v2/pipeline/$LATEST_PIPELINE_ID/workflow?circle-token=b045cdea55d0e571a208181e4366f4c62c4016ce")
-LATEST_WORKFLOW_ID=$(echo "$LATEST_WORKFLOWS" | jq '.items' | jq '.[0]' | jq -r '.id')
-LATEST_JOBS=$(curl -L "https://circleci.com/api/v2/workflow/$LATEST_WORKFLOW_ID/job?circle-token=b045cdea55d0e571a208181e4366f4c62c4016ce")
-JOB_NUMBER=$(echo "$LATEST_JOBS" | jq '.items' | jq '.[] | select(.name=="angular_testing")' | jq -r '.job_number')
+PREVIOUS_PIPELINE_ID=$(echo "$PIPELINES" | jq '.items' | jq '.[1]' | jq -r '.id')
+PREVIOUS_WORKFLOWS=$(curl -L "https://circleci.com/api/v2/pipeline/$PREVIOUS_PIPELINE_ID/workflow?circle-token=b045cdea55d0e571a208181e4366f4c62c4016ce")
+PREVIOUS_WORKFLOW_ID=$(echo "$PREVIOUS_WORKFLOWS" | jq '.items' | jq '.[0]' | jq -r '.id')
+PREVIOUS_JOBS=$(curl -L "https://circleci.com/api/v2/workflow/$PREVIOUS_WORKFLOW_ID/job?circle-token=b045cdea55d0e571a208181e4366f4c62c4016ce")
+JOB_NUMBER=$(echo "$PREVIOUS_JOBS" | jq '.items' | jq '.[] | select(.name=="angular_testing")' | jq -r '.job_number')
 ARTIFACT_DETAILS=$(curl -L "https://circleci.com/api/v2/project/github/lindy-immerse/circleci-beginner/$JOB_NUMBER/artifacts?circle-token=b045cdea55d0e571a208181e4366f4c62c4016ce")
 ARTIFACT_URL=$(echo "$ARTIFACT_DETAILS" | jq '.items' | jq '.[0]' | jq -r '.url')
 ARTIFACT=$(curl -L $ARTIFACT_URL)
@@ -38,6 +38,8 @@ echo -e "---> ARTIFACT\n$ARTIFACT"
 echo -e "\n"
 
 ##------ DETERMINE NEW THRESHOLD ------##
+
+
 
 ##------ SAVE NEW THRESHOLD ------##
 
