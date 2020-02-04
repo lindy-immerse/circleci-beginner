a809456b81bfadefb3298d840bd847d8df79544b
Lindy Brits <lindy.brits@immerse.io>
2020-02-04 11:51:35 +0000

    Changes made.

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -25,28 +25,28 @@ count=0
 ARTIFACT=$(cat "./data/karma_threshold.json" | jq '.')
 echo "$ARTIFACT"
 
-#for key in $(echo $CURRENT_THRESHOLD | jq -r 'keys[]'); do
-#    CURRENT_VALUE=$(echo $CURRENT_THRESHOLD | jq -r --arg key "$key" '.[$key]')
-#    PREVIOUS_VALUE=$(echo $ARTIFACT | jq -r --arg key "$key" '.[$key]')
-#
-#    if [[ $count != 0 ]]; then
-#        THRESHOLD="${THRESHOLD},"
-#    fi
-#
-#    if [[ "$CURRENT_VALUE" > "$PREVIOUS_VALUE" ]];
-#    then
-#        THRESHOLD="${THRESHOLD}\n\"$key\": $CURRENT_VALUE"
-#    else
-#        THRESHOLD="${THRESHOLD}\n\"$key\": $PREVIOUS_VALUE"
-#    fi
-#
-#    ((count=count+1))
-#done
-#
-#THRESHOLD="${THRESHOLD}\n}"
-#THRESHOLD_JS="module.exports = ${THRESHOLD};"
-#
-###------ SAVE NEW THRESHOLD ------##
-#
-#echo -e "$THRESHOLD" > "$PWD/data/karma_stats.json"
-#echo -e "$THRESHOLD_JS" > "$PWD/data/karma_stats.js"
+for key in $(echo $CURRENT_THRESHOLD | jq -r 'keys[]'); do
+    CURRENT_VALUE=$(echo $CURRENT_THRESHOLD | jq -r --arg key "$key" '.[$key]')
+    PREVIOUS_VALUE=$(echo $ARTIFACT | jq -r --arg key "$key" '.[$key]')
+
+    if [[ $count != 0 ]]; then
+        THRESHOLD="${THRESHOLD},"
+    fi
+
+    if [[ "$CURRENT_VALUE" > "$PREVIOUS_VALUE" ]];
+    then
+        THRESHOLD="${THRESHOLD}\n\"$key\": $CURRENT_VALUE"
+    else
+        THRESHOLD="${THRESHOLD}\n\"$key\": $PREVIOUS_VALUE"
+    fi
+
+    ((count=count+1))
+done
+
+THRESHOLD="${THRESHOLD}\n}"
+THRESHOLD_JS="module.exports = ${THRESHOLD};"
+
+##------ SAVE NEW THRESHOLD ------##
+
+echo -e "$THRESHOLD" > "$PWD/data/karma_threshold.json"
+echo -e "$THRESHOLD_JS" > "$PWD/data/karma_threshold.js"
