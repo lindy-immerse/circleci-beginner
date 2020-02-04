7658e7a663325dea34f5e83b7c7051b055cbeaf4
Lindy Brits <lindy.brits@immerse.io>
2020-02-04 11:46:57 +0000

    Changes made.

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -17,55 +17,36 @@ CURRENT_THRESHOLD=$(jq -n \
         --arg branches "$BRANCHES" \
         '{lines: $lines, statements: $statements, functions: $functions, branches: $branches}')
 
-##------ PULL THRESHOLD ARTIFACT ------##
-
-PIPELINES=$(curl -L "https://circleci.com/api/v2/project/github/lindy-immerse/circleci-beginner/pipeline?circle-token=b045cdea55d0e571a208181e4366f4c62c4016ce")
-PREVIOUS_PIPELINE_ID=$(echo "$PIPELINES" | jq '.items' | jq '.[1]' | jq -r '.id')
-PREVIOUS_WORKFLOWS=$(curl -L "https://circleci.com/api/v2/pipeline/$PREVIOUS_PIPELINE_ID/workflow?circle-token=b045cdea55d0e571a208181e4366f4c62c4016ce")
-PREVIOUS_WORKFLOW_ID=$(echo "$PREVIOUS_WORKFLOWS" | jq '.items' | jq '.[0]' | jq -r '.id')
-PREVIOUS_JOBS=$(curl -L "https://circleci.com/api/v2/workflow/$PREVIOUS_WORKFLOW_ID/job?circle-token=b045cdea55d0e571a208181e4366f4c62c4016ce")
-JOB_NUMBER=$(echo "$PREVIOUS_JOBS" | jq '.items' | jq '.[] | select(.name=="angular_testing")' | jq -r '.job_number')
-ARTIFACTS=$(curl -L "https://circleci.com/api/v2/project/github/lindy-immerse/circleci-beginner/$JOB_NUMBER/artifacts?circle-token=b045cdea55d0e571a208181e4366f4c62c4016ce")
-ARTIFACT_URL=$(echo "$ARTIFACTS" | jq '.items' | jq '.[1]' | jq -r '.url')
-ARTIFACT=$(curl -L $ARTIFACT_URL)
-
-echo -e "\n"
-echo "---> LATEST PIPELINE ---> $LATEST_PIPELINE_ID"
-echo "---> LATEST WORKFLOW ---> $LATEST_WORKFLOW_ID"
-echo "---> JOB NUMBER ---> $JOB_NUMBER"
-echo "---> ARTIFACT URL ---> $ARTIFACT_URL"
-echo -e "---> ARTIFACT\n$ARTIFACT"
-echo -e "\n"
-
 ##------ DETERMINE NEW THRESHOLD ------##
 
 THRESHOLD="{"
 count=0
 
+ARTIFACT=$(cat "./data/karma_threshold.json" | jq '.')
 echo "$ARTIFACT"
 
-for key in $(echo $CURRENT_THRESHOLD | jq -r 'keys[]'); do
-    CURRENT_VALUE=$(echo $CURRENT_THRESHOLD | jq -r --arg key "$key" '.[$key]')
-    PREVIOUS_VALUE=$(echo $ARTIFACT | jq -r --arg key "$key" '.[$key]')
-
-    if [[ $count != 0 ]]; then
-        THRESHOLD="${THRESHOLD},"
-    fi
-
-    if [[ "$CURRENT_VALUE" > "$PREVIOUS_VALUE" ]];
-    then
-        THRESHOLD="${THRESHOLD}\n\"$key\": $CURRENT_VALUE"
-    else
-        THRESHOLD="${THRESHOLD}\n\"$key\": $PREVIOUS_VALUE"
-    fi
-
-    ((count=count+1))
-done
-
-THRESHOLD="${THRESHOLD}\n}"
-THRESHOLD_JS="module.exports = ${THRESHOLD};"
-
-##------ SAVE NEW THRESHOLD ------##
-
-echo -e "$THRESHOLD" > "$PWD/data/karma_stats.json"
-echo -e "$THRESHOLD_JS" > "$PWD/data/karma_stats.js"
+#for key in $(echo $CURRENT_THRESHOLD | jq -r 'keys[]'); do
+#    CURRENT_VALUE=$(echo $CURRENT_THRESHOLD | jq -r --arg key "$key" '.[$key]')
+#    PREVIOUS_VALUE=$(echo $ARTIFACT | jq -r --arg key "$key" '.[$key]')
+#
+#    if [[ $count != 0 ]]; then
+#        THRESHOLD="${THRESHOLD},"
+#    fi
+#
+#    if [[ "$CURRENT_VALUE" > "$PREVIOUS_VALUE" ]];
+#    then
+#        THRESHOLD="${THRESHOLD}\n\"$key\": $CURRENT_VALUE"
+#    else
+#        THRESHOLD="${THRESHOLD}\n\"$key\": $PREVIOUS_VALUE"
+#    fi
+#
+#    ((count=count+1))
+#done
+#
+#THRESHOLD="${THRESHOLD}\n}"
+#THRESHOLD_JS="module.exports = ${THRESHOLD};"
+#
+###------ SAVE NEW THRESHOLD ------##
+#
+#echo -e "$THRESHOLD" > "$PWD/data/karma_stats.json"
+#echo -e "$THRESHOLD_JS" > "$PWD/data/karma_stats.js"
