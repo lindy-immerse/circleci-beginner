2527c1967b1db25f57472fd745cda059b90beccf
Lindy Brits <lindy.brits@immerse.io>
2020-02-04 12:47:26 +0000

    Changes made.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,7 +3,7 @@
 ##------ PULL THRESHOLD ARTIFACT ------##
 
 PIPELINES=$(curl -L "https://circleci.com/api/v2/project/github/lindy-immerse/circleci-beginner/pipeline?circle-token=b045cdea55d0e571a208181e4366f4c62c4016ce")
-PREVIOUS_PIPELINE_ID=$(echo "$PIPELINES" | jq '.items' | jq '.[0]' | jq -r '.id')
+PREVIOUS_PIPELINE_ID=$(echo "$PIPELINES" | jq '.items' | jq '.[1]' | jq -r '.id')
 PREVIOUS_WORKFLOWS=$(curl -L "https://circleci.com/api/v2/pipeline/$PREVIOUS_PIPELINE_ID/workflow?circle-token=b045cdea55d0e571a208181e4366f4c62c4016ce")
 PREVIOUS_WORKFLOW_ID=$(echo "$PREVIOUS_WORKFLOWS" | jq '.items' | jq '.[0]' | jq -r '.id')
 PREVIOUS_JOBS=$(curl -L "https://circleci.com/api/v2/workflow/$PREVIOUS_WORKFLOW_ID/job?circle-token=b045cdea55d0e571a208181e4366f4c62c4016ce")
